8a41039c09de9a72d83e6ec5724f20d0e97b55f4
Egor Kozelskij <ekozelskiy@integrotechnologies.ru>
2023-11-01 00:01:38 +0300

    feat(app): demo categories icons

--- app/rest/init.go
+++ app/rest/init.go
@@ -1,12 +1,14 @@
 package rest
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 	"go.uber.org/dig"
 	"microservice/app/core"
 	"net/http"
+	"os"
 	"reflect"
 	"runtime"
 	"strings"
@@ -73,10 +75,44 @@ func InitImageServer(log core.Logger) {
 	port := viper.GetString("img.port")
 	imgPath := viper.GetString("img.path")
 
+	// Also serve categories list
+	entries, err := os.ReadDir(imgPath + "/categories")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	categories := make(map[string][]string)
+	for _, e := range entries {
+		if e.IsDir() {
+			if _, ok := categories[e.Name()]; !ok {
+				categories[e.Name()] = []string{}
+			}
+		}
+	}
+	for c := range categories {
+		entries, err := os.ReadDir(imgPath + "/categories/" + c)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		for _, e := range entries {
+			if !e.IsDir() {
+				categories[c] = append(categories[c], e.Name())
+			}
+		}
+	}
+
 	fs := http.FileServer(http.Dir(imgPath))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
+	http.HandleFunc("/static-list/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		json.NewEncoder(w).Encode(categories)
+	})
+
 	log.Info("Image server listening on port :" + port)
-	err := http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
